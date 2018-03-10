688a4e182bcd47645c1986e4732612ef85afa507
minjun.ha <haminjun0@gmail.com>
2018-03-10 19:31:46 +0900

    Socket IO 플레이어 이동 관련 emit 추가

--- Server/app.js
+++ Server/app.js
@@ -1,10 +1,24 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.get('/', function(req, res){
+    print('hello');
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    socket.on('move', function (data) {
+        socket.broadcast.emit('hi',data); // 자신을 제외하고 다른 클라이언트에게 보냄
+        //socket.emit('hi',data)
+        // socket.emit('toclient',data); // 해당 클라이언트에게만 보냄. 다른 클라이언트에 보낼려면?
+        //console.log('Message from client :'+data.msg);
+
+        console.log('move' + data);
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
 });
