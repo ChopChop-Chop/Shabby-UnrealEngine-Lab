10c08593f39016659f17395c5dfc924d80cc6558
minjun.ha <haminjun0@gmail.com>
2018-03-17 13:40:56 +0900

    Mac 버전에서 SocketIO 사용할 수 있게 수정

---
+++ MunchkinExample/Intermediate/Build/Mac/UE4Editor/Inc/MunchkinExample/MunchkinExampleCharacter.gen.cpp
@@ -0,0 +1,112 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#include "MunchkinExampleCharacter.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeMunchkinExampleCharacter() {}
+// Cross Module References
+	MUNCHKINEXAMPLE_API UClass* Z_Construct_UClass_AMunchkinExampleCharacter_NoRegister();
+	MUNCHKINEXAMPLE_API UClass* Z_Construct_UClass_AMunchkinExampleCharacter();
+	ENGINE_API UClass* Z_Construct_UClass_ACharacter();
+	UPackage* Z_Construct_UPackage__Script_MunchkinExample();
+	ENGINE_API UClass* Z_Construct_UClass_UCameraComponent_NoRegister();
+	ENGINE_API UClass* Z_Construct_UClass_USpringArmComponent_NoRegister();
+// End Cross Module References
+	void AMunchkinExampleCharacter::StaticRegisterNativesAMunchkinExampleCharacter()
+	{
+	}
+	UClass* Z_Construct_UClass_AMunchkinExampleCharacter_NoRegister()
+	{
+		return AMunchkinExampleCharacter::StaticClass();
+	}
+	UClass* Z_Construct_UClass_AMunchkinExampleCharacter()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			static UObject* (*const DependentSingletons[])() = {
+				(UObject* (*)())Z_Construct_UClass_ACharacter,
+				(UObject* (*)())Z_Construct_UPackage__Script_MunchkinExample,
+			};
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[] = {
+				{ "HideCategories", "Navigation" },
+				{ "IncludePath", "MunchkinExampleCharacter.h" },
+				{ "ModuleRelativePath", "MunchkinExampleCharacter.h" },
+			};
+#endif
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_BaseLookUpRate_MetaData[] = {
+				{ "Category", "Camera" },
+				{ "ModuleRelativePath", "MunchkinExampleCharacter.h" },
+				{ "ToolTip", "Base look up/down rate, in deg/sec. Other scaling may affect final rate." },
+			};
+#endif
+			static const UE4CodeGen_Private::FFloatPropertyParams NewProp_BaseLookUpRate = { UE4CodeGen_Private::EPropertyClass::Float, "BaseLookUpRate", RF_Public|RF_Transient|RF_MarkAsNative, 0x0010000000020015, 1, nullptr, STRUCT_OFFSET(AMunchkinExampleCharacter, BaseLookUpRate), METADATA_PARAMS(NewProp_BaseLookUpRate_MetaData, ARRAY_COUNT(NewProp_BaseLookUpRate_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_BaseTurnRate_MetaData[] = {
+				{ "Category", "Camera" },
+				{ "ModuleRelativePath", "MunchkinExampleCharacter.h" },
+				{ "ToolTip", "Base turn rate, in deg/sec. Other scaling may affect final turn rate." },
+			};
+#endif
+			static const UE4CodeGen_Private::FFloatPropertyParams NewProp_BaseTurnRate = { UE4CodeGen_Private::EPropertyClass::Float, "BaseTurnRate", RF_Public|RF_Transient|RF_MarkAsNative, 0x0010000000020015, 1, nullptr, STRUCT_OFFSET(AMunchkinExampleCharacter, BaseTurnRate), METADATA_PARAMS(NewProp_BaseTurnRate_MetaData, ARRAY_COUNT(NewProp_BaseTurnRate_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_FollowCamera_MetaData[] = {
+				{ "AllowPrivateAccess", "true" },
+				{ "Category", "Camera" },
+				{ "EditInline", "true" },
+				{ "ModuleRelativePath", "MunchkinExampleCharacter.h" },
+				{ "ToolTip", "Follow camera" },
+			};
+#endif
+			static const UE4CodeGen_Private::FObjectPropertyParams NewProp_FollowCamera = { UE4CodeGen_Private::EPropertyClass::Object, "FollowCamera", RF_Public|RF_Transient|RF_MarkAsNative, 0x00400000000a001d, 1, nullptr, STRUCT_OFFSET(AMunchkinExampleCharacter, FollowCamera), Z_Construct_UClass_UCameraComponent_NoRegister, METADATA_PARAMS(NewProp_FollowCamera_MetaData, ARRAY_COUNT(NewProp_FollowCamera_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_CameraBoom_MetaData[] = {
+				{ "AllowPrivateAccess", "true" },
+				{ "Category", "Camera" },
+				{ "EditInline", "true" },
+				{ "ModuleRelativePath", "MunchkinExampleCharacter.h" },
+				{ "ToolTip", "Camera boom positioning the camera behind the character" },
+			};
+#endif
+			static const UE4CodeGen_Private::FObjectPropertyParams NewProp_CameraBoom = { UE4CodeGen_Private::EPropertyClass::Object, "CameraBoom", RF_Public|RF_Transient|RF_MarkAsNative, 0x00400000000a001d, 1, nullptr, STRUCT_OFFSET(AMunchkinExampleCharacter, CameraBoom), Z_Construct_UClass_USpringArmComponent_NoRegister, METADATA_PARAMS(NewProp_CameraBoom_MetaData, ARRAY_COUNT(NewProp_CameraBoom_MetaData)) };
+			static const UE4CodeGen_Private::FPropertyParamsBase* const PropPointers[] = {
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_BaseLookUpRate,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_BaseTurnRate,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_FollowCamera,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_CameraBoom,
+			};
+			static const FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
+				TCppClassTypeTraits<AMunchkinExampleCharacter>::IsAbstract,
+			};
+			static const UE4CodeGen_Private::FClassParams ClassParams = {
+				&AMunchkinExampleCharacter::StaticClass,
+				DependentSingletons, ARRAY_COUNT(DependentSingletons),
+				0x00800080u,
+				nullptr, 0,
+				PropPointers, ARRAY_COUNT(PropPointers),
+				"Game",
+				&StaticCppClassTypeInfo,
+				nullptr, 0,
+				METADATA_PARAMS(Class_MetaDataParams, ARRAY_COUNT(Class_MetaDataParams))
+			};
+			UE4CodeGen_Private::ConstructUClass(OuterClass, ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AMunchkinExampleCharacter, 650068970);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AMunchkinExampleCharacter(Z_Construct_UClass_AMunchkinExampleCharacter, &AMunchkinExampleCharacter::StaticClass, TEXT("/Script/MunchkinExample"), TEXT("AMunchkinExampleCharacter"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AMunchkinExampleCharacter);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ MunchkinExample/Intermediate/Build/Mac/UE4Editor/Inc/MunchkinExample/MunchkinExampleGameMode.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef MUNCHKINEXAMPLE_MunchkinExampleGameMode_generated_h
+#error "MunchkinExampleGameMode.generated.h already included, missing '#pragma once' in MunchkinExampleGameMode.h"
+#endif
+#define MUNCHKINEXAMPLE_MunchkinExampleGameMode_generated_h
+
+#define MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_12_RPC_WRAPPERS
+#define MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMunchkinExampleGameMode(); \
+	friend MUNCHKINEXAMPLE_API class UClass* Z_Construct_UClass_AMunchkinExampleGameMode(); \
+public: \
+	DECLARE_CLASS(AMunchkinExampleGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/MunchkinExample"), MUNCHKINEXAMPLE_API) \
+	DECLARE_SERIALIZER(AMunchkinExampleGameMode) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAMunchkinExampleGameMode(); \
+	friend MUNCHKINEXAMPLE_API class UClass* Z_Construct_UClass_AMunchkinExampleGameMode(); \
+public: \
+	DECLARE_CLASS(AMunchkinExampleGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/MunchkinExample"), MUNCHKINEXAMPLE_API) \
+	DECLARE_SERIALIZER(AMunchkinExampleGameMode) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	MUNCHKINEXAMPLE_API AMunchkinExampleGameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMunchkinExampleGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(MUNCHKINEXAMPLE_API, AMunchkinExampleGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMunchkinExampleGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	MUNCHKINEXAMPLE_API AMunchkinExampleGameMode(AMunchkinExampleGameMode&&); \
+	MUNCHKINEXAMPLE_API AMunchkinExampleGameMode(const AMunchkinExampleGameMode&); \
+public:
+
+
+#define MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	MUNCHKINEXAMPLE_API AMunchkinExampleGameMode(AMunchkinExampleGameMode&&); \
+	MUNCHKINEXAMPLE_API AMunchkinExampleGameMode(const AMunchkinExampleGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(MUNCHKINEXAMPLE_API, AMunchkinExampleGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMunchkinExampleGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AMunchkinExampleGameMode)
+
+
+#define MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_12_PRIVATE_PROPERTY_OFFSET
+#define MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_9_PROLOG
+#define MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_12_RPC_WRAPPERS \
+	MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_12_INCLASS \
+	MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID MunchkinExample_Source_MunchkinExample_MunchkinExampleGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
